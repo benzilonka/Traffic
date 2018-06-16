2eded4ee7a963c10e3259133dc82d5b5ec60ccf2
galycsr1 <galycsr1@gmail.com>
2018-06-16 20:55:10 +0300

    Add files via upload

--- test/Data_Analysis_test.py
+++ test/Data_Analysis_test.py
@@ -1,12 +1,11 @@
 import unittest
-
-from server.Data_Analysis import *
+import Data_Analysis
 
 class TestDataAnalysis (unittest.TestCase):
 
     def test_addTTC(self):
         vehicles = {'objects' : ({ 'bounding_box' : [100, 200, 0,0], 'speed' : 10, 'distance' : 0, 'ttc': 0},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10, 'distance' : 0, 'ttc': 0},{ 'bounding_box' : [300, 200, 0,0], 'speed' : 10, 'distance' : 0, 'ttc': 0})}
-        result = add_ttc(vehicles,5)
+        result = Data_Analysis.add_ttc(vehicles,5)
         self.assertEqual(result['objects'][0]['distance'],0)
         self.assertEqual(result['objects'][1]['distance'],0)
         self.assertEqual(result['objects'][2]['distance'],0)
@@ -14,7 +13,7 @@ class TestDataAnalysis (unittest.TestCase):
         self.assertEqual(result['objects'][1]['ttc'],-1)
         self.assertEqual(result['objects'][2]['ttc'],-1)
         vehicles = {'objects' : ({ 'bounding_box' : [100, 200, 0,0], 'speed' : 20, 'distance' : 0, 'ttc': 0},{ 'bounding_box' : [100, 550, 0,0], 'speed' : 30, 'distance' : 0, 'ttc': 0},{ 'bounding_box' : [100, 350, 0,0], 'speed' : 10, 'distance' : 0, 'ttc': 0})}
-        result = add_ttc(vehicles,5)
+        result = Data_Analysis.add_ttc(vehicles,5)
         self.assertEqual(result['objects'][0]['distance'],30)
         self.assertEqual(result['objects'][1]['distance'],0)
         self.assertEqual(result['objects'][2]['distance'],40)
@@ -22,7 +21,7 @@ class TestDataAnalysis (unittest.TestCase):
         self.assertEqual(result['objects'][1]['ttc'],-1)
         self.assertEqual(result['objects'][2]['ttc'],-1)
         vehicles = {'objects' : ({ 'bounding_box' : [100, 50, 0,0], 'speed' : 103, 'distance' : 0, 'ttc': 0},{ 'bounding_box' : [100, 200, 0,0], 'speed' : 97, 'distance' : 0, 'ttc': 0},{ 'bounding_box' : [100, 500, 0,0], 'speed' : 93, 'distance' : 0, 'ttc': 0})}
-        result = add_ttc(vehicles,5)
+        result = Data_Analysis.add_ttc(vehicles,5)
         self.assertEqual(result['objects'][0]['distance'],30)
         self.assertEqual(result['objects'][1]['distance'],60)
         self.assertEqual(result['objects'][2]['distance'],0)
@@ -30,7 +29,7 @@ class TestDataAnalysis (unittest.TestCase):
         self.assertEqual(result['objects'][1]['ttc'],3)
         self.assertEqual(result['objects'][2]['ttc'],-1)
         vehicles = {'objects' : ({ 'bounding_box' : [100, 50, 0,0], 'speed' : 0, 'distance' : 0, 'ttc': 0},{ 'bounding_box' : [100, 200, 0,0], 'speed' : 6, 'distance' : 0, 'ttc': 0},{ 'bounding_box' : [100, 500, 0,0], 'speed' : 5, 'distance' : 0, 'ttc': 0})}
-        result = add_ttc(vehicles,5)
+        result = Data_Analysis.add_ttc(vehicles,5)
         self.assertEqual(result['objects'][0]['distance'],30)
         self.assertEqual(result['objects'][1]['distance'],60)
         self.assertEqual(result['objects'][2]['distance'],0)
@@ -42,39 +41,39 @@ class TestDataAnalysis (unittest.TestCase):
 
     def test_calcTTC(self):
         vehicles = ({ 'bounding_box' : [100, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [300, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [400, 200, 0,0], 'speed' : 10})
-        self.assertEqual(calc_ttc(vehicles[0],vehicles,5),-1)
+        self.assertEqual(Data_Analysis.calc_ttc(vehicles[0],vehicles,5),-1)
         vehicles = ({ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10})
-        self.assertEqual(calc_ttc(vehicles[0],vehicles,5),-1)
+        self.assertEqual(Data_Analysis.calc_ttc(vehicles[0],vehicles,5),-1)
         vehicles = ({ 'bounding_box' : [200, 400, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 100, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 130, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10})
-        self.assertEqual(calc_ttc(vehicles[0],vehicles,5),-1)
+        self.assertEqual(Data_Analysis.calc_ttc(vehicles[0],vehicles,5),-1)
         vehicles = ({ 'bounding_box' : [200, 200, 0,0], 'speed' : 0},{ 'bounding_box' : [200, 300, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 250, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 205, 0,0], 'speed' : 10})
-        self.assertEqual(calc_ttc(vehicles[0],vehicles,5),-1)
+        self.assertEqual(Data_Analysis.calc_ttc(vehicles[0],vehicles,5),-1)
         vehicles = ({ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 400, 0,0], 'speed' : 9})
-        self.assertEqual(calc_ttc(vehicles[0],vehicles,5),-1)
+        self.assertEqual(Data_Analysis.calc_ttc(vehicles[0],vehicles,5),-1)
         vehicles = ({ 'bounding_box' : [200, 200, 0,0], 'speed' : 20},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 400, 0,0], 'speed' : 10})
-        self.assertEqual(calc_ttc(vehicles[0],vehicles,5),4/5)
+        self.assertEqual(Data_Analysis.calc_ttc(vehicles[0],vehicles,5),4/5)
         vehicles = ({ 'bounding_box' : [200, 200, 0,0], 'speed' : 20},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 200, 0,0], 'speed' : 10},{ 'bounding_box' : [200, 400, 0,0], 'speed' : 12})
-        self.assertEqual(calc_ttc(vehicles[0],vehicles,5),1)
+        self.assertEqual(Data_Analysis.calc_ttc(vehicles[0],vehicles,5),1)              
     
     def test_getDistance(self):
         vehicles = ({ 'bounding_box' : [100, 200, 0,0]},{ 'bounding_box' : [200, 200, 0,0]},{ 'bounding_box' : [300, 200, 0,0]},{ 'bounding_box' : [400, 200, 0,0]})
-        self.assertEqual(get_distance(vehicles[0],vehicles),0)
+        self.assertEqual(Data_Analysis.get_distance(vehicles[0],vehicles),0)
         vehicles = ({ 'bounding_box' : [200, 200, 0,0]},{ 'bounding_box' : [200, 200, 0,0]},{ 'bounding_box' : [200, 200, 0,0]},{ 'bounding_box' : [200, 200, 0,0]})
-        self.assertEqual(get_distance(vehicles[0],vehicles),0)
+        self.assertEqual(Data_Analysis.get_distance(vehicles[0],vehicles),0)
         vehicles = ({ 'bounding_box' : [200, 400, 0,0]},{ 'bounding_box' : [200, 100, 0,0]},{ 'bounding_box' : [200, 130, 0,0]},{ 'bounding_box' : [200, 200, 0,0]})
-        self.assertEqual(get_distance(vehicles[0],vehicles),0)
+        self.assertEqual(Data_Analysis.get_distance(vehicles[0],vehicles),0)
         vehicles = ({ 'bounding_box' : [200, 200, 0,0]},{ 'bounding_box' : [200, 300, 0,0]},{ 'bounding_box' : [200, 250, 0,0]},{ 'bounding_box' : [200, 205, 0,0]})
-        self.assertEqual(get_distance(vehicles[0],vehicles),1)
+        self.assertEqual(Data_Analysis.get_distance(vehicles[0],vehicles),1)
         vehicles = ({ 'bounding_box' : [200, 200, 0,0]},{ 'bounding_box' : [200, 200, 0,0]},{ 'bounding_box' : [200, 200, 0,0]},{ 'bounding_box' : [200, 330, 0,0]})
-        self.assertEqual(get_distance(vehicles[0],vehicles),130/5)
+        self.assertEqual(Data_Analysis.get_distance(vehicles[0],vehicles),130/5)        
         
     
     def test_areInSameLane(self):
-        self.assertTrue(are_in_same_lane(85,80))
-        self.assertFalse(are_in_same_lane(0,30))
-        self.assertTrue(are_in_same_lane(0,22))
-        self.assertFalse(are_in_same_lane(40,120))
-        self.assertFalse(are_in_same_lane(-30,5))
+        self.assertTrue(Data_Analysis.are_in_same_lane(85,80))
+        self.assertFalse(Data_Analysis.are_in_same_lane(0,30))
+        self.assertTrue(Data_Analysis.are_in_same_lane(0,22))
+        self.assertFalse(Data_Analysis.are_in_same_lane(40,120))
+        self.assertFalse(Data_Analysis.are_in_same_lane(-30,5))
 
     def test_is_vehicle_passed_in_red_light(self): 
         lanes = {"right": [49, 60], "forward": [38, 49], "left": [27, 38]}
@@ -84,15 +83,15 @@ class TestDataAnalysis (unittest.TestCase):
         frame4 = {'objects': [{'confidence': 0.72, 'type': 'bus', 'static': True, 'created_at': '2017-12-28 07:39:00.933165', 'times_lost_by_convnet': 0, 'speed': 2.367083004736198, 'lost': False, 'alert_tags': [], 'tracking_id': 26598, 'bounding_box': [48, 239, 92, 132], 'new': False, 'counted': False}],  'light_status': {'right' :'red','forward' :'red','left':'red'},'frame_index': 6348747}
         frame5 = {'objects': [{'confidence': 0.72, 'type': 'bus', 'static': True, 'created_at': '2017-12-28 07:39:00.933165', 'times_lost_by_convnet': 0, 'speed': 0.0, 'lost': False, 'alert_tags': [], 'tracking_id': 26598, 'bounding_box': [48, 240, 91, 131], 'new': False, 'counted': False}],  'light_status': {'right' :'red','forward' :'red','left':'red'}, 'frame_index': 6348753} 
         
-        frame1 = is_vehicle_passed_in_red_light(frame1, None, 200, lanes)
+        frame1 = Data_Analysis.is_vehicle_passed_in_red_light(frame1, None, 200, lanes)
         self.assertFalse(frame1['objects'][0]['passed_in_red'])
-        frame2 = is_vehicle_passed_in_red_light(frame2, frame1, 200, lanes)
+        frame2 = Data_Analysis.is_vehicle_passed_in_red_light(frame2, frame1, 200, lanes)
         self.assertFalse(frame2['objects'][0]['passed_in_red'])
-        frame3 = is_vehicle_passed_in_red_light(frame3, frame2, 235, lanes)
-        self.assertFalse(frame3['objects'][0]['passed_in_red'])
-        frame4 = is_vehicle_passed_in_red_light(frame4, frame3, 238, lanes)
+        frame3 = Data_Analysis.is_vehicle_passed_in_red_light(frame3, frame2, 235, lanes)
+        self.assertTrue(frame3['objects'][0]['passed_in_red'])
+        frame4 = Data_Analysis.is_vehicle_passed_in_red_light(frame4, frame3, 238, lanes)
         self.assertTrue(frame4['objects'][0]['passed_in_red'])
-        frame5= is_vehicle_passed_in_red_light(frame5, frame4, 238, lanes)
+        frame5= Data_Analysis.is_vehicle_passed_in_red_light(frame5, frame4, 238, lanes)
         self.assertFalse(frame5['objects'][0]['passed_in_red'])
 
     def test_add_zigzag_count(self):
@@ -104,19 +103,19 @@ class TestDataAnalysis (unittest.TestCase):
         {'objects': [{'confidence': 0.72, 'type': 'bus', 'static': True, 'created_at': '2017-12-28 07:39:00.933165', 'times_lost_by_convnet': 0, 'speed': 0.0, 'lost': False, 'alert_tags': [], 'tracking_id': 26598, 'bounding_box': [525, 232, 91, 131], 'new': False, 'counted': False}, {'confidence': 0.82, 'type': 'car', 'static': False, 'created_at': '2017-12-28 07:40:02.417256', 'times_lost_by_convnet': 8, 'speed': 8.204357854743904, 'lost': False, 'alert_tags': [], 'tracking_id': 26609, 'bounding_box': [372, 331, 70, 52], 'new': False, 'counted': True}, {'confidence': 0.77, 'type': 'car', 'static': False, 'created_at': '2017-12-28 07:40:03.798450', 'times_lost_by_convnet': 1, 'speed': 18.51134117483986, 'lost': False, 'alert_tags': [], 'tracking_id': 26610, 'bounding_box': [414, 290, 49, 35], 'new': False, 'counted': False}], 'frame_index': 6348753},  
         {'objects': [{'confidence': 0.7, 'type': 'bus', 'static': True, 'created_at': '2017-12-28 07:39:00.933165', 'times_lost_by_convnet': 0, 'speed': 0.0, 'lost': False, 'alert_tags': [], 'tracking_id': 26598, 'bounding_box': [525, 232, 91, 134], 'new': False, 'counted': False}, {'confidence': 0.82, 'type': 'car', 'static': False, 'created_at': '2017-12-28 07:40:02.417256', 'times_lost_by_convnet': 10, 'speed': 8.614160325384688, 'lost': False, 'alert_tags': [], 'tracking_id': 26609, 'bounding_box': [370, 332, 70, 52], 'new': False, 'counted': True}, {'confidence': 0.76, 'type': 'car', 'static': False, 'created_at': '2017-12-28 07:40:03.798450', 'times_lost_by_convnet': 1, 'speed': 23.706263884080034, 'lost': False, 'alert_tags': [], 'tracking_id': 26610, 'bounding_box': [410, 292, 52, 35], 'new': False, 'counted': False}], 'frame_index': 6348759}
         ]
-        frames[0] = add_zigzag_count(frames[0], None, 1, 0)
+        frames[0] = Data_Analysis.add_zigzag_count(frames[0], None, 1, 0)
         self.assertTrue(frames[0]['objects'][0]['change_lane_count']==0)
         self.assertFalse(frames[0]['objects'][0]['against_direction_flag'])
-        frames[1] = add_zigzag_count(frames[1], frames[0], 1, 0)
+        frames[1] = Data_Analysis.add_zigzag_count(frames[1], frames[0], 1, 0)
         self.assertTrue(frames[1]['objects'][0]['change_lane_count']==1)
         self.assertFalse(frames[1]['objects'][0]['against_direction_flag'])
-        frames[2] = add_zigzag_count(frames[2], frames[1], 1, 0)
+        frames[2] = Data_Analysis.add_zigzag_count(frames[2], frames[1], 1, 0)
         self.assertTrue(frames[2]['objects'][0]['change_lane_count']==1)
         self.assertTrue(frames[2]['objects'][0]['against_direction_flag'])
-        frames[3] = add_zigzag_count(frames[3], frames[2], 1, 0)
+        frames[3] = Data_Analysis.add_zigzag_count(frames[3], frames[2], 1, 0)
         self.assertTrue(frames[3]['objects'][0]['change_lane_count']==1)
         self.assertFalse(frames[3]['objects'][0]['against_direction_flag'])
-        frames[4]= add_zigzag_count(frames[4], frames[3], 1, 0)
+        frames[4]= Data_Analysis.add_zigzag_count(frames[4], frames[3], 1, 0)
         self.assertTrue(frames[4]['objects'][1]['change_lane_count']==4)
         self.assertFalse(frames[4]['objects'][1]['against_direction_flag'])
     
